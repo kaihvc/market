01e8fed78d2bc6af52d6349e7bf1901f455fb80c
kaihvc <47830342+kaihvc@users.noreply.github.com>
2022-04-17 14:23:27 -0600

    Changed sample to tsx and used div

--- src/components/Publish/S3/example.ts
+++
@@ -1,5 +0,0 @@
-import ReactElement from 'react'
-
-export default function SampleElement() {
-  return <h1>This is your sample element</h1>
-}
---
+++ src/components/Publish/S3/example.tsx
@@ -0,0 +1,10 @@
+import ReactElement from 'react'
+import Markdown from '../../@shared/Markdown'
+
+export default function SampleElement() {
+  return (
+    <div>
+      This is your sample component
+    </div>
+  )
+}
